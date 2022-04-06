5c8769f9abac44ffdfbec793b5079fbdcc35d6f4
Sujay Jayakar <sujayakar@convex.dev>
2022-04-05 20:51:53 -0700

    Initial commit

---
+++ convex/newQuestion.ts
@@ -0,0 +1,5 @@
+import { mutation } from "@convex-dev/server";
+
+export default mutation(async ({db}, text: string) => {
+    db.insert("questions", { text, votes: 0 });
+})
---
+++ convex/questions.ts
@@ -0,0 +1,38 @@
+import { Id, mutation, query } from "@convex-dev/server";
+
+export type Question = {
+    id: Id,
+    text: string,
+    votes: number,
+}
+
+export const loadQuestions = query(async ({db}): Promise<Question[]> => {
+    const ret: Question[] = [];
+    for (const question of await db.table("questions").collect()) {
+        ret.push({
+            id: question._id,
+            text: question.text,
+            votes: question.votes,
+        })
+    }
+    ret.sort((a, b) => b.votes - a.votes);
+    return ret;
+});
+
+export const upvote = mutation(async ({db}, id: Id) => {
+    const doc: Question | undefined = await db.get(id);
+    if (!doc) {
+        return;
+    }
+    doc.votes += 1;
+    db.update(id, doc);
+})
+
+export const downvote = mutation(async ({db}, id: Id) => {
+    const doc: Question | undefined = await db.get(id);
+    if (!doc) {
+        return;
+    }
+    doc.votes -= 1;
+    db.update(id, doc);
+})
